02190ba56033384e61af98063ce2b500e6e43c2c
Geunsik Lim <leemgs@gmail.com>
2024-09-08 11:50:34 +0900

    added input.txt for image gallery
    Signed-off-by: Geunsik Lim <leemgs@gmail.com>

---
+++ 2023/input_generate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ls -1 *.jpg > ./input.txt
+echo -e "The input.txt file is generated as follows."
+cat ./input.txt
+
+echo -e "sbumitting a new commit ..........."
+input add . 
+git commit -a -s -m "added input.txt for image gallery"
+git push origin
