bfcf18533b7dfc87dad2dde4d0a22e2d61c38395
Geunsik Lim <leemgs@gmail.com>
2024-09-10 13:00:54 +0900

    added input.txt for image gallery
    Signed-off-by: Geunsik Lim <leemgs@gmail.com>

--- input_generate.sh
+++ input_generate.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-ls -1 *.jpg > ./input.txt
-echo -e "The input.txt file is generated as follows."
-cat ./input.txt
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide a year as an argument."
+    echo "Usage: $0 <year>"
+    exit 1
+fi
 
-echo -e "sbumitting a new commit ..........."
+year_dir=$1
+
+echo -e "[DEBUG] Fetching change from the GitHub repo..."
+git pull
+cd ./wor_folder/
+ls -1 *.jpg > ./input.txt
+echo -e "[DEBUG] The input.txt file is generated as follows."
+cat ./input.txt
+echo -e "[DEBUG] commiting new changes ..........."
 git add . 
 git commit -a -s -m "added input.txt for image gallery"
+echo -e "[DEBUG] pushing new change into the GitHub repo..."
 git push origin
+cd ..
