f56cbb76458eef786081cc752d6096e51ee6fce7
Geunsik Lim <leemgs@gmail.com>
2024-09-10 13:01:46 +0900

    added input.txt for image gallery
    Signed-off-by: Geunsik Lim <leemgs@gmail.com>

--- input_generate.sh
+++ input_generate.sh
@@ -9,15 +9,15 @@ fi
 
 year_dir=$1
 
-echo -e "[DEBUG] Fetching change from the GitHub repo..."
+echo -e "[DEBUG]### Fetching change from the GitHub repo..."
 git pull
 cd ./wor_folder/
 ls -1 *.jpg > ./input.txt
-echo -e "[DEBUG] The input.txt file is generated as follows."
+echo -e "[DEBUG]### The input.txt file is generated as follows."
 cat ./input.txt
-echo -e "[DEBUG] commiting new changes ..........."
+echo -e "[DEBUG]### commiting new changes ..........."
 git add . 
 git commit -a -s -m "added input.txt for image gallery"
-echo -e "[DEBUG] pushing new change into the GitHub repo..."
+echo -e "[DEBUG]### pushing new change into the GitHub repo..."
 git push origin
 cd ..
